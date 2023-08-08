d986bb24f4b5c5826fb8e0be4c831dde0fde7013
Guilherme Deon de Faria <guibyn@gmail.com>
2023-08-08 20:15:50 -0300

    Ex 7

--- code/day_2/quests/Guilherme Deon de Faria/script_07.py
+++ code/day_2/quests/Guilherme Deon de Faria/script_07.py
@@ -1,3 +1,13 @@
 '''
 7 - Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
 '''
+
+option = str(input("Digite o sexo: F=Feminino, M = Masculino: \n"))
+option = option.lower()
+
+if option == 'f':
+    print("\nFeminino")
+elif option == 'm':
+    print("\nMasculino")
+else:
+    print("Sexo invalido")
