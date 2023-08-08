8375047827fd95e5a8fa75fee3b8fee0924ce1ab
Guilherme Deon de Faria <guibyn@gmail.com>
2023-08-08 20:07:05 -0300

    Ex 5

--- code/day_2/quests/Guilherme Deon de Faria/script_05.py
+++ code/day_2/quests/Guilherme Deon de Faria/script_05.py
@@ -2,3 +2,14 @@
 5 - Faça um programa com uma função chamada somaImposto. A função possui dois parâmetros formais: taxaImposto, que é a quantia de imposto sobre vendas expressa em
 porcentagem e custo, que é o custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas.
 '''
+
+#takes percentage as number without "%", or example 50% = 50
+def somaImposto(taxaImposto, custo):
+    return custo + (custo * (taxaImposto/100))
+
+valor = 10
+print(valor)
+
+#taxaimposto = 5%, valor = 10
+valor = somaImposto(5,valor)
+print(valor)
