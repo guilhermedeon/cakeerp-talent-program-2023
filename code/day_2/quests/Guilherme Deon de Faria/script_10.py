78dbf9a02329fcf2713773a1d412e42dc95c0ef5
Guilherme Deon de Faria <guibyn@gmail.com>
2023-08-08 20:55:03 -0300

    Ex 10

--- code/day_2/quests/Guilherme Deon de Faria/script_10.py
+++ code/day_2/quests/Guilherme Deon de Faria/script_10.py
@@ -11,3 +11,45 @@ F. o percentual de aumento aplicado;
 G. o valor do aumento;
 H. o novo salário, após o aumento.
 '''
+
+def calcular_percentual_reajuste(salario):
+    reajuste = 0
+    if salario <= 280:
+        reajuste = 20
+    elif salario <= 700:
+        reajuste = 15
+    elif salario <= 1500:
+        reajuste = 10
+    else:
+        reajuste = 5
+    return reajuste/100
+
+def calcular_salario_mais_reajuste(salario,reajuste):
+    return salario + (salario * reajuste)
+
+
+i = 0
+salarioInicial = 0
+
+while i < 1:
+    try:
+        salarioInicial = round(float(input("\nSalario atual do colaborador: \n")),2)
+        i = i + 1
+    except:
+        print("Valor invalido")
+
+percentualReajuste = calcular_percentual_reajuste(salarioInicial)
+salarioReajustado = round(
+    calcular_salario_mais_reajuste(
+        salarioInicial,
+        percentualReajuste
+        ),
+    2)
+valorAumento = round(salarioReajustado - salarioInicial,2)
+
+print("\nSalario inicial = " + str(salarioInicial))
+print("Percentual de aumento aplicado = " + str(percentualReajuste*100) + "%")
+print("Valor do aumento = " + str(valorAumento))
+print("Novo salario = " + str(salarioReajustado) + "\n")
+
+
