1fd3b077b5a76197b2a209a35bed6d95fdef14a6
Guilherme Deon de Faria <guibyn@gmail.com>
2023-08-08 19:51:28 -0300

    Ex 3

--- code/day_2/quests/Guilherme Deon de Faria/script_03.py
+++ code/day_2/quests/Guilherme Deon de Faria/script_03.py
@@ -8,3 +8,44 @@ E. "Já trabalhou com a vítima?"
 O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. Se a pessoa responder positivamente a 2 questões ela deve ser classificada
 como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
 '''
+
+respostas = list()
+perguntas = (
+    "Telefonou para a vítima?",
+    "Esteve no local do crime?",
+    "Mora perto da vítima?",
+    "Devia para a vítima?",
+    "Já trabalhou com a vítima?")
+
+i = 0
+while i < 5:
+    try:
+        print("\nResponda SIM ou NAO - Questao: " + str(i+1))
+        resposta = input(perguntas[i]+"\n")
+        resposta = resposta.lower()
+        if resposta  == "nao" or resposta == "sim":
+            i = i + 1
+            respostas.append(resposta)
+        else:
+            raise Exception()
+    except:
+        print("Resposta invalida")
+
+
+print("\nRespostas:")
+contagemSim = 0
+for i in range(0,len(respostas)):
+    print(perguntas[i])
+    print(respostas[i])
+    if respostas[i] == "sim":
+        contagemSim = contagemSim + 1
+
+print("\nVeredito:")
+if contagemSim < 2:
+    print("Inocente!")
+if contagemSim == 2:
+    print("Suspeito!")
+if contagemSim == 3 or contagemSim == 4:
+    print("Cumplice!")
+if contagemSim == 5:
+    print("Assassino!")
