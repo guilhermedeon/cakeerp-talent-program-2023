d9f50f66696612227e0b1a721bfd1d1d6d5ebdd9
Guilherme Deon de Faria <guibyn@gmail.com>
2023-08-08 19:10:24 -0300

    File creations

---
+++ code/day_2/quests/Guilherme Deon de Faria/script_01.py
@@ -0,0 +1,7 @@
+'''
+1 - Faça um Programa que leia um vetor de 5 números inteiros e mostre-os.
+'''
+
+minha_lista = list()
+
+print(type(minha_lista))
---
+++ code/day_2/quests/Guilherme Deon de Faria/script_10.py
@@ -0,0 +1,13 @@
+'''
+10 - As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o programa que
+calculará os reajustes. Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário
+atual:
+A. salários até R$ 280,00 (incluindo) : aumento de 20%
+B. salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
+C. salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
+D. salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
+E. o salário antes do reajuste;
+F. o percentual de aumento aplicado;
+G. o valor do aumento;
+H. o novo salário, após o aumento.
+'''
