db7165006488f1faff048f4fc5aa4b50acede0a0
Guilherme Deon de Faria <guibyn@gmail.com>
2023-08-08 20:12:26 -0300

    Ex 6

--- code/day_2/quests/Guilherme Deon de Faria/script_06.py
+++ code/day_2/quests/Guilherme Deon de Faria/script_06.py
@@ -1,3 +1,18 @@
 '''
 6 - Faça um Programa que peça dois números e imprima o maior deles.
 '''
+
+try:
+    value_1 = float(input("Primeiro valor:\n"))
+    value_2 = float(input("Segundo valor:\n"))
+
+    print("\n")
+    if value_1 > value_2:
+        print(value_1)
+    if value_2 >= value_1:
+        print(value_2)
+
+except:
+    print("Valor invalido")
+
+
